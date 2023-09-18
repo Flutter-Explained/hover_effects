fb4b349ef14c96ec67035a0a3bbddccfffb2f7d2
Max Weber <weber.max.1988@gmail.com>
2023-09-18 11:28:28 +0200

    [Feature] List view of different hover effects that will be created in the future

---
+++ lib/01_glass_hover/glass_hover_effect.dart
@@ -0,0 +1,141 @@
+/// Tutorial for the first video
+/// Glass Hover effect
+
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+
+class GlassHoverEffect extends StatefulWidget {
+  static void navigate(context) {
+    Navigator.of(context).push(
+      MaterialPageRoute(
+        builder: (context) => const GlassHoverEffect(),
+      ),
+    );
+  }
+
+  const GlassHoverEffect({super.key});
+
+  @override
+  State<GlassHoverEffect> createState() => _GlassHoverEffectState();
+}
+
+class _GlassHoverEffectState extends State<GlassHoverEffect>
+    with SingleTickerProviderStateMixin {
+  late final AnimationController _controller = AnimationController(
+    duration: const Duration(milliseconds: 600),
+    vsync: this,
+  );
+
+  late final Animation<Offset> _animation = Tween<Offset>(
+    begin: const Offset(1.1, 0),
+    end: Offset.zero,
+  ).animate(
+    CurvedAnimation(
+      parent: _controller,
+      curve: Curves.easeInOut,
+    ),
+  );
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("Glass Hover Effect")),
+      backgroundColor: const Color(0xFF3B6D83),
+      body: Center(
+        child: MouseRegion(
+          onEnter: (event) => _controller.forward(),
+          onExit: (event) => _controller.reverse(),
+          child: ClipRRect(
+            clipBehavior: Clip.hardEdge,
+            child: SizedBox(
+              height: 420,
+              width: 320,
+              child: Stack(
+                children: [
+                  Container(
+                    decoration: const BoxDecoration(
+                      borderRadius: BorderRadius.all(Radius.circular(25)),
+                      image: DecorationImage(
+                        image: NetworkImage(
+                          "https://github.com/Flutter-Explained/hover_effects/blob/main/assets/isfahan.jpg",
+                        ),
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                  ),
+                  SlideTransition(
+                    position: _animation,
+                    child: ClipRRect(
+                      clipBehavior: Clip.hardEdge,
+                      child: BackdropFilter(
+                        filter: ImageFilter.blur(sigmaX: 6, sigmaY: 6),
+                        child: Padding(
+                          padding: const EdgeInsets.all(32.0),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              const SizedBox(height: 50),
+                              SelectableText(
+                                "Isfahan",
+                                style: Theme.of(context)
+                                    .textTheme
+                                    .titleLarge!
+                                    .copyWith(color: Colors.white),
+                              ),
+                              SelectableText(
+                                "Where History and Beauty Unite",
+                                style: Theme.of(context)
+                                    .textTheme
+                                    .titleSmall!
+                                    .copyWith(color: Colors.white),
+                              ),
+                              const SizedBox(height: 25),
+                              SelectableText(
+                                "Isfahan, Iran's cultural gem, boasts stunning architecture, like the Imam Mosque and Naqsh-e Jahan Square. Rich history and vibrant culture await visitors.",
+                                style: Theme.of(context)
+                                    .textTheme
+                                    .bodyMedium!
+                                    .copyWith(color: Colors.white),
+                              ),
+                              Expanded(
+                                child: Row(
+                                  children: [
+                                    ElevatedButton(
+                                      child: const Text(
+                                        "Discover the Jewel of Iran",
+                                      ),
+                                      onPressed: () {},
+                                    ),
+                                  ],
+                                ),
+                              )
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                  IgnorePointer(
+                    child: SizedBox(
+                      width: 320,
+                      height: 420,
+                      child: DecoratedBox(
+                        decoration: BoxDecoration(
+                          border: Border.all(color: Colors.white, width: 6),
+                          borderRadius: const BorderRadius.all(
+                            Radius.circular(25),
+                          ),
+                        ),
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
-import 'dart:ui';
-
 import 'package:flutter/material.dart';
+import 'package:hover_effect/01_glass_hover/glass_hover_effect.dart';
 
 void main() {
   runApp(const MainApp());
@@ -13,116 +12,40 @@ class MainApp extends StatefulWidget {
   State<MainApp> createState() => _MainAppState();
 }
 
-class _MainAppState extends State<MainApp> with SingleTickerProviderStateMixin {
-  late final AnimationController _controller = AnimationController(
-    duration: const Duration(milliseconds: 600),
-    vsync: this,
-  );
+class EffectDemo {
+  String title;
+  void Function(BuildContext context) navigation;
+  EffectDemo({required this.title, required this.navigation});
+}
 
-  late final Animation<Offset> _animation = Tween<Offset>(
-    begin: const Offset(1.1, 0),
-    end: Offset.zero,
-  ).animate(
-    CurvedAnimation(
-      parent: _controller,
-      curve: Curves.easeInOut,
+class _MainAppState extends State<MainApp> with SingleTickerProviderStateMixin {
+  List<EffectDemo> demos = [
+    EffectDemo(
+      title: "Glass Hover Effect",
+      navigation: GlassHoverEffect.navigate,
     ),
-  );
+  ];
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData.light().copyWith(
+        useMaterial3: true,
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepOrange),
+      ),
       home: Scaffold(
-        backgroundColor: const Color(0xFF3B6D83),
-        body: Center(
-          child: MouseRegion(
-            onEnter: (event) => _controller.forward(),
-            onExit: (event) => _controller.reverse(),
-            child: ClipRRect(
-              clipBehavior: Clip.hardEdge,
-              child: SizedBox(
-                height: 420,
-                width: 320,
-                child: Stack(
-                  children: [
-                    Container(
-                      decoration: const BoxDecoration(
-                        borderRadius: BorderRadius.all(Radius.circular(25)),
-                        image: DecorationImage(
-                          image: AssetImage("assets/isfahan.jpg"),
-                          fit: BoxFit.cover,
-                        ),
-                      ),
-                    ),
-                    SlideTransition(
-                      position: _animation,
-                      child: ClipRRect(
-                        clipBehavior: Clip.hardEdge,
-                        child: BackdropFilter(
-                          filter: ImageFilter.blur(sigmaX: 6, sigmaY: 6),
-                          child: Padding(
-                            padding: const EdgeInsets.all(32.0),
-                            child: Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                const SizedBox(height: 50),
-                                SelectableText(
-                                  "Isfahan",
-                                  style: Theme.of(context)
-                                      .textTheme
-                                      .titleLarge!
-                                      .copyWith(color: Colors.white),
-                                ),
-                                SelectableText(
-                                  "Where History and Beauty Unite",
-                                  style: Theme.of(context)
-                                      .textTheme
-                                      .titleSmall!
-                                      .copyWith(color: Colors.white),
-                                ),
-                                const SizedBox(height: 25),
-                                SelectableText(
-                                  "Isfahan, Iran's cultural gem, boasts stunning architecture, like the Imam Mosque and Naqsh-e Jahan Square. Rich history and vibrant culture await visitors.",
-                                  style: Theme.of(context)
-                                      .textTheme
-                                      .bodyMedium!
-                                      .copyWith(color: Colors.white),
-                                ),
-                                Expanded(
-                                  child: Row(
-                                    children: [
-                                      ElevatedButton(
-                                        child: const Text(
-                                            "Discover the Jewel of Iran"),
-                                        onPressed: () {},
-                                      ),
-                                    ],
-                                  ),
-                                )
-                              ],
-                            ),
-                          ),
-                        ),
-                      ),
-                    ),
-                    IgnorePointer(
-                      child: SizedBox(
-                        width: 320,
-                        height: 420,
-                        child: DecoratedBox(
-                          decoration: BoxDecoration(
-                            border: Border.all(color: Colors.white, width: 6),
-                            borderRadius: const BorderRadius.all(
-                              Radius.circular(25),
-                            ),
-                          ),
-                        ),
-                      ),
-                    )
-                  ],
-                ),
-              ),
-            ),
-          ),
+        appBar: AppBar(title: const Text("Hover Effects")),
+        body: ListView.separated(
+          itemBuilder: (context, index) {
+            return ListTile(
+              title: Text(demos[index].title),
+              onTap: () => demos[index].navigation(context),
+            );
+          },
+          separatorBuilder: (context, index) {
+            return const Divider(height: 1);
+          },
+          itemCount: demos.length,
         ),
       ),
     );
