36d294f95dd6a1d5ad4bd89bc5d4d725444db0cd
Max Weber <weber.max.1988@gmail.com>
2023-09-21 20:12:52 +0200

    Upgraded flutter version and added social media icons

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
+import 'package:feather_icons/feather_icons.dart';
 import 'package:flutter/material.dart';
 import 'package:hover_effect/01_glass_hover/glass_hover_effect.dart';
 import 'package:hover_effect/02_github_card/github_card.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 void main() {
   runApp(const MainApp());
@@ -20,6 +22,29 @@ class EffectDemo {
 }
 
 class _MainAppState extends State<MainApp> with SingleTickerProviderStateMixin {
+  List<SocialMediaIcon> socials = [
+    SocialMediaIcon(
+      unparsedUrl: "https://youtube.com/@flutterexplained",
+      iconData: FeatherIcons.youtube,
+      tooltip: "Flutter Explained YouTube Channel",
+    ),
+    SocialMediaIcon(
+      unparsedUrl: "https://flutter-explained.dev",
+      iconData: FeatherIcons.home,
+      tooltip: "Flutter Explained Website",
+    ),
+    SocialMediaIcon(
+      unparsedUrl: "https://github.com/md-weber",
+      iconData: FeatherIcons.github,
+      tooltip: "GitHub",
+    ),
+    SocialMediaIcon(
+      unparsedUrl: "https://www.linkedin.com/in/flutterexp",
+      iconData: FeatherIcons.linkedin,
+      tooltip: "GitHub",
+    )
+  ];
+
   List<EffectDemo> demos = [
     EffectDemo(
       title: "Glass Hover Effect",
@@ -40,19 +65,62 @@ class _MainAppState extends State<MainApp> with SingleTickerProviderStateMixin {
       ),
       home: Scaffold(
         appBar: AppBar(title: const Text("Hover Effects")),
-        body: ListView.separated(
-          itemBuilder: (context, index) {
-            return ListTile(
-              title: Text(demos[index].title),
-              onTap: () => demos[index].navigation(context),
-            );
-          },
-          separatorBuilder: (context, index) {
-            return const Divider(height: 1);
-          },
-          itemCount: demos.length,
+        body: Column(
+          children: [
+            Expanded(
+              child: ListView.separated(
+                itemBuilder: (context, index) {
+                  return ListTile(
+                    title: Text(demos[index].title),
+                    onTap: () => demos[index].navigation(context),
+                  );
+                },
+                separatorBuilder: (context, index) {
+                  return const Divider(height: 1);
+                },
+                itemCount: demos.length,
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(24.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  const Text("Follow me:"),
+                  for (var social in socials)
+                    IconButton(
+                      onPressed: () => openURL(social.unparsedUrl),
+                      icon: Icon(social.iconData),
+                      tooltip: social.tooltip,
+                    ),
+                ],
+              ),
+            )
+          ],
         ),
       ),
     );
   }
+
+  openURL(String unparsedURL) async {
+    var url = Uri.parse(unparsedURL);
+    if (!await launchUrl(
+      url,
+      mode: LaunchMode.externalApplication,
+    )) {
+      throw Exception('Could not launch $url');
+    }
+  }
+}
+
+class SocialMediaIcon {
+  String unparsedUrl;
+  String? tooltip;
+  IconData iconData;
+
+  SocialMediaIcon({
+    required this.unparsedUrl,
+    required this.iconData,
+    this.tooltip,
+  });
 }
